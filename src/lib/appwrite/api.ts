8f4544891635bf7dc68f7b6b25d0b7c805a193d0
Kelechi Otugbuali <hilakechi@gmail.com>
2023-11-28 20:26:44 +0100

    CreatePost section build

--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,7 +1,7 @@
 import { ID, Query } from 'appwrite'
 
-import { INewUser } from '@/types'
-import { account, appwriteConfig, avatars, databases } from './config'
+import { INewPost, INewUser } from '@/types'
+import { account, appwriteConfig, avatars, databases, storage } from './config'
 
 export async function createUserAccount(user: INewUser) {
   try {
@@ -12,7 +12,7 @@ export async function createUserAccount(user: INewUser) {
       user.name,
     )
 
-    if(!newAccount) throw Error;
+    if (!newAccount) throw Error
 
     const avatarUrl = avatars.getInitials(user.name)
 
@@ -22,13 +22,9 @@ export async function createUserAccount(user: INewUser) {
       email: newAccount.email,
       username: user.username,
       imageUrl: avatarUrl,
-
-
-
-
     })
 
-    return newUser;
+    return newUser
   } catch (error) {
     console.log(error)
     return error
@@ -36,65 +32,189 @@ export async function createUserAccount(user: INewUser) {
 }
 
 export async function saveUserToDB(user: {
-  accountId: string,
-  email: string,
-  name: string,
-  imageUrl:URL,
-  username?: string,
-
-
+  accountId: string
+  email: string
+  name: string
+  imageUrl: URL
+  username?: string
 }) {
-
- try {
-  const newUser = await databases.createDocument(
-     appwriteConfig.databaseId,
-     appwriteConfig.userCollectionId,
-     ID.unique(),
-     user,
-  )
-  return newUser
- } catch (error) {
-console.log(error)
- }
+  try {
+    const newUser = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      user,
+    )
+    return newUser
+  } catch (error) {
+    console.log(error)
+  }
 }
 
-export async function signInAccount(user: {email: string; password: string;}){
+// export async function signInAccount(user: { email: string; password: string }) {
+//   try {
+//     const session = await account.createEmailSession(user.email, user.password)
+//     localStorage.setItem('sessionId', session.userId)
+//     return session
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+export async function signInAccount(user: { email: string; password: string }) {
   try {
-
+    // console.log("signing innnn");
     const session = await account.createEmailSession(user.email, user.password)
+    // console.log("session:", session);
+    localStorage.setItem('sessionId', session.userId) //userId stored as a sessionId -> purpose of storing is after some time of user log in, when try to fetch useId on reload the appWrite is unable to fetch resulting 401
     return session
   } catch (error) {
     console.log(error)
   }
 }
 
-export async function getCurrentUser(){
+// export async function getCurrentUser() {
+//   try {
+//     const currentAccount = await account.get()
+//     if (!currentAccount) throw Error
+//     const currentUser = await databases.listDocuments(
+//       appwriteConfig.databaseId,
+//       appwriteConfig.userCollectionId,
+//       [Query.equal('accountId', currentAccount.$id)],
+//     )
+
+//     if (!currentUser) throw Error
+
+//     return currentUser.documents[0]
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
+
+export async function getCurrentUser() {
   try {
-    const currentAccount = await account.get();
-    if(!currentAccount) throw Error
-const currentUser = await databases.listDocuments(
-  appwriteConfig.databaseId,
-  appwriteConfig.userCollectionId,
-  [Query.equal('accountId', currentAccount.$id)]
-)
+    // console.log("userId: ", localStorage.getItem("sessionId"));
+    // const currentAccount = await account.get();
+    const currentAccountId = localStorage.getItem('sessionId')
 
-if(!currentUser) throw Error
+    if (currentAccountId !== null) {
+      // if (!currentAccount) throw Error;
 
+      const currentUser = await databases.listDocuments(
+        appwriteConfig.databaseId,
+        appwriteConfig.userCollectionId,
+        // [Query.equal("accountId", currentAccount.$id)]
+        [Query.equal('accountId', currentAccountId)],
+      )
 
-return currentUser.documents[0]
+      // console.log("currentUser:", currentUser);
+
+      if (!currentUser) throw Error
+
+      return currentUser.documents[0]
+    }
   } catch (error) {
     console.log(error)
-
   }
 }
 
-
-
-export async function signOutAccount(){
+export async function signOutAccount() {
   try {
     const session = await account.deleteSession('current')
     return session
   } catch (error) {
     console.log(error)
   }
+}
+
+// ============================== CREATE POST
+export async function createPost(post: INewPost) {
+  try {
+    // Upload file to appwrite storage
+    const uploadedFile = await uploadFile(post.file[0])
+
+    if (!uploadedFile) throw Error
+
+    // Get file url
+    const fileUrl = getFilePreview(uploadedFile.$id)
+    if (!fileUrl) {
+      await deleteFile(uploadedFile.$id)
+      throw Error
+    }
+
+    // Convert tags into array
+    const tags = post.tags?.replace(/ /g, '').split(',') || []
+
+    // Create post
+    const newPost = await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.postCollectionId,
+      ID.unique(),
+      {
+        creator: post.userId,
+        caption: post.caption,
+        imageUrl: fileUrl,
+        imageId: uploadedFile.$id,
+        location: post.location,
+        tags: tags,
+      },
+    )
+
+    if (!newPost) {
+      await deleteFile(uploadedFile.$id)
+      throw Error
+    }
+
+    return newPost
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function uploadFile(file: File) {
+  try {
+    const uploadedFile = await storage.createFile(
+      appwriteConfig.storageId,
+      ID.unique(),
+      file,
+    )
+
+    return uploadedFile
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getFilePreview(fileId: string) {
+  try {
+    const fileUrl = storage.getFilePreview(
+      appwriteConfig.storageId,
+      fileId,
+      2000,
+      2000,
+      'top',
+      100,
+    )
+    return fileUrl
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function deleteFile(fileId: string) {
+  try {
+    await storage.deleteFile(appwriteConfig.storageId, fileId)
+    return { status: 'ok' }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export async function getRecentPosts() {
+  const posts = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.postCollectionId,
+    [Query.orderDesc('$createdAt'), Query.limit(20)],
+  )
+  if (!posts) throw Error
+  return posts
 }
--- src/lib/react-query/queriesAndMutation.ts
+++ src/lib/react-query/queriesAndMutation.ts
@@ -1,8 +1,9 @@
 import{
 useQuery,useQueryClient,useInfiniteQuery, useMutation
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
-import { INewUser } from '@/types'
+import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from '../appwrite/api'
+import { INewPost, INewUser } from '@/types'
+import { QUERY_KEYS } from './queryKeys'
 
 
 export const useCreateUserAccount = ()=>{
@@ -20,11 +21,28 @@ export const useSignInAccount = ()=>{
     })
 }
 
-
-
-
 export const useSignOutAccount = ()=>{
     return useMutation({
-            mutationFn:  signOutAccount
+      mutationFn:  signOutAccount
     })
+}
+
+export const useCreatePost = ()=>{
+    const queryClient = useQueryClient()
+
+    return useMutation({
+        mutationFn: (post: INewPost) => createPost(post),
+        onSuccess: () =>{
+            queryClient.invalidateQueries({
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS]
+            })
+        }
+    })
+}
+
+export const useGetRecentPosts = () => {
+     return useQuery({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        queryFn: getRecentPosts
+     })
 }
